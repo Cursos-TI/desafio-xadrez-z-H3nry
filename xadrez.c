4a97199cda210221da01a3bb84eb11cfa7d1bc1c
z-H3nry <henrique75souto9@gmail.com>
2025-02-06 14:09:50 +0000

    Nível Mestre feito

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,216 @@
 #include <stdio.h>
+#include <string.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+#define BispoMovimentoMaximo 5
+#define TorreMovimentoMaximo 5
+#define RainhaMovimentoMaximo 8
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+// A Função que toma conta da movimentação de cada pesa da mesa de xadrez.
+// Sendo possível escolher a direção de movimentação de acordo com o  jogo de xadrez em sir.
+// EX: o bispo se move diagonalmente, num formato de 'X', então o jogador pode escolher em se mover diagonalmente para esquerda ou direta.
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+void Mover_Bispo()
+{
+    int LadoEscolhido; // 1 - esquerda , 2 - direita
+    int TotalDeCasas;  // Quantas casas vai ser percorridas
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf("||Mover Bispo Pra Qual Direção? \n");
+    printf("||1. Esquerda \n");
+    printf("||2. Direita \n");
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    scanf("%d", &LadoEscolhido);
+    (LadoEscolhido > 2 || LadoEscolhido < 1) ? (printf("\n \n \n Opção desconhecida. \n")) : (LadoEscolhido);
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    // Caso a opção seja invalida, pare o codigo aqui.
+    if (LadoEscolhido > 2 || LadoEscolhido < 1)
+    {
+        return;
+    }
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    printf("||Quantas Casas? \n");
+    scanf("%d", &TotalDeCasas);
 
+    // Aviso de correção, caso o jogador tenha inserido um numero maior que o "maximo" de casas que pode ser percorrido.
+    (TotalDeCasas > BispoMovimentoMaximo) ? (printf("||%d é maior que o maximo de casas que pode ser percorrida! -|corrigindo valor para: %d|- \n", TotalDeCasas, BispoMovimentoMaximo)) : (TotalDeCasas);
+    (TotalDeCasas > BispoMovimentoMaximo) ? (TotalDeCasas = BispoMovimentoMaximo) : (TotalDeCasas);
+
+    printf("\n||[Bispo] Caminho percorrido:");
+    if (LadoEscolhido <= 2 || LadoEscolhido >= 1)
+    {
+        for (int i = 0; i < TotalDeCasas; i++)
+        {
+            char Lado[25];
+
+            (LadoEscolhido > 1) ? (strcpy(Lado, "Direita")) : (strcpy(Lado, "Esquerda"));
+
+            printf("\n %s \n Cima", Lado);
+        }
+    }
+}
+
+void Mover_Torre()
+{
+    int LadoEscolhido; // 1 - esquerda , 2 - direita , 3 - cima/reto
+    int TotalDeCasas;  // Quantas casas vai ser percorridas
+
+    printf("||Mover a Torre Pra Qual Direção? \n");
+    printf("||1. Esquerda \n");
+    printf("||2. Direita \n");
+    printf("||3. Cima \n");
+
+    scanf("%d", &LadoEscolhido);
+    (LadoEscolhido > 3 || LadoEscolhido < 1) ? (printf("\n \n \n Opção desconhecida. \n")) : (LadoEscolhido);
+
+    // Caso a opção seja invalida, pare o codigo aqui.
+    if (LadoEscolhido > 3 || LadoEscolhido < 1)
+    {
+        return;
+    }
+
+    printf("||Quantas Casas? \n");
+    scanf("%d", &TotalDeCasas);
+
+    // Aviso de correção, caso o jogador tenha inserido um numero maior que o "maximo" de casas que pode ser percorrido.
+    (TotalDeCasas > TorreMovimentoMaximo) ? (printf("||%d é maior que o maximo de casas que pode ser percorrida! -|corrigindo valor para: %d|- \n", TotalDeCasas, TorreMovimentoMaximo)) : (TotalDeCasas);
+    (TotalDeCasas > TorreMovimentoMaximo) ? (TotalDeCasas = TorreMovimentoMaximo) : (TotalDeCasas);
+
+    printf("\n||[Torre] Caminho percorrido:");
+    if (LadoEscolhido <= 2 || LadoEscolhido >= 1)
+    {
+        for (int i = 0; i < TotalDeCasas; i++)
+        {
+            char Lado[25];
+
+            (LadoEscolhido == 1) ? (strcpy(Lado, "Esquerda")) : ((LadoEscolhido == 2) ? (strcpy(Lado, "Direita")) : ((LadoEscolhido == 3) ? (strcpy(Lado, "Cima")) : (strcpy(Lado, "Direção Nula"))));
+
+            printf("\n %s", Lado);
+        }
+    }
+}
+
+void Mover_Rainha()
+{
+    int LadoEscolhido; // 1 - esquerda , 2 - direita , 3 - cima/reto , 4 - baixo
+    int TotalDeCasas;  // Quantas casas vai ser percorridas
+
+    printf("||Mover a Rainha Pra Qual Direção? \n");
+    printf("||1. Esquerda \n");
+    printf("||2. Direita \n");
+    printf("||3. Cima \n");
+    printf("||4. Baixo \n");
+
+    scanf("%d", &LadoEscolhido);
+    (LadoEscolhido > 4 || LadoEscolhido < 1) ? (printf("\n \n \n Opção desconhecida. \n")) : (LadoEscolhido);
+
+    // Caso a opção seja invalida, pare o codigo aqui.
+    if (LadoEscolhido > 4 || LadoEscolhido < 1)
+    {
+        return;
+    }
+
+    printf("||Quantas Casas? \n");
+    scanf("%d", &TotalDeCasas);
+
+    // Aviso de correção, caso o jogador tenha inserido um numero maior que o "maximo" de casas que pode ser percorrido.
+    (TotalDeCasas > RainhaMovimentoMaximo) ? (printf("||%d é maior que o maximo de casas que pode ser percorrida! -|corrigindo valor para: %d|- \n", TotalDeCasas, RainhaMovimentoMaximo)) : (TotalDeCasas);
+    (TotalDeCasas > RainhaMovimentoMaximo) ? (TotalDeCasas = RainhaMovimentoMaximo) : (TotalDeCasas);
+
+    printf("\n||[Rainha] Caminho percorrido:");
+    if (LadoEscolhido <= 2 || LadoEscolhido >= 1)
+    {
+        for (int i = 0; i < TotalDeCasas; i++)
+        {
+            char Lado[25];
+
+            (LadoEscolhido == 1) ? (strcpy(Lado, "Esquerda")) : ((LadoEscolhido == 2) ? (strcpy(Lado, "Direita")) : ((LadoEscolhido == 3) ? (strcpy(Lado, "Cima")) : ((LadoEscolhido == 4) ? (strcpy(Lado, "Baixo")) : (strcpy(Lado, "Direção Nula")))));
+
+            printf("\n %s", Lado);
+        }
+    }
+}
+
+void Mover_Cavalo()
+{
+    int LadoEscolhido; // 1 - esquerda , 2 - direita
+
+    printf("||Mover o Cavalo Pra Qual Direção? \n");
+    printf("||1. Esquerda \n");
+    printf("||2. Direita \n");
+
+    scanf("%d", &LadoEscolhido);
+    (LadoEscolhido > 2 || LadoEscolhido < 1) ? (printf("\n \n \n Opção desconhecida. \n")) : (LadoEscolhido);
+
+    // Caso a opção seja invalida, pare o codigo aqui.
+    if (LadoEscolhido > 2 || LadoEscolhido < 1)
+    {
+        return;
+    }
+
+    printf("\n||[Cavalo] Caminho percorrido:");
+    if (LadoEscolhido <= 2 || LadoEscolhido >= 1)
+    {
+        for (int i = 1; i < 4; i++)
+        {
+            char Lado[25];
+
+            (i <= 2) ? (strcpy(Lado, "Cima")) : ((LadoEscolhido > 1) ? (strcpy(Lado, "Direita")) : (strcpy(Lado, "Esquerda")));
+
+            printf("\n %s", Lado);
+        }
+    }
+}
+
+int main()
+{
+
+    int Leave = 0; //Caso o jogador queira parar o jogo, "leave" estara valendo 1.
+
+    do {
+
+    int Menu_SelectedOption; //Opção que o jogador escolheu
+
+    //Amostrar o menu pro jogador, perguntando qual pesa ele pretende escolher.
+    printf("\n \n||==================================================|| \n");
+    printf("||Qual peça pretende mover? \n");
+    printf("||1. Bispo \n");
+    printf("||2. Torre \n");
+    printf("||3. Rainha \n");
+    printf("||4. Cavalo \n");
+    printf("||5. Nenhum (Sair do jogo) \n");
+    printf("||==================================================|| \n");
+
+    scanf("%d",&Menu_SelectedOption);
+
+    switch (Menu_SelectedOption)
+    {
+    case 1:
+    Mover_Bispo();
+    break;
+
+    case 2:
+    Mover_Torre();
+    break;
+
+    case 3:
+    Mover_Rainha();
+    break;
+
+    case 4:
+    Mover_Cavalo();
+    break;
+    
+    case 5:
+    printf("\n ||Jogo Encerrado.");
+    Leave = 1;
+    break;
+
+    default:
+    printf("\n ||Opção Desconhecida. \n");
+    break;
+    }
+
+    } while (Leave < 1);
+    
     return 0;
 }
